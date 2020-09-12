c99f0153e4d7cacf0649da7e47ce95ff9806f6c9
abhishek24591 <abhidsgsg@gmail.com>
2020-09-13 00:03:32 +0530

    Second commit

--- TestVgrant_Automation/src/test/java/TestVagrant_Automation/TestVgrant_Automation/NDTV.java
+++ TestVgrant_Automation/src/test/java/TestVagrant_Automation/TestVgrant_Automation/NDTV.java
@@ -1,5 +1,7 @@
 package TestVagrant_Automation.TestVgrant_Automation;
 
+import java.io.FileReader;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
@@ -7,12 +9,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NDTV {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "F:\\Downloads\\chromedriver_win32_version85\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		FileReader propertiesFile = new FileReader("F:\\TestVagrant\\TestVgrant_Automation\\Object_repository\\object.properties");
+		Properties p = new Properties(propertiesFile);
+		String url = p.get(appurl);
 		driver.get("https://www.ndtv.com/");
 		
 		
